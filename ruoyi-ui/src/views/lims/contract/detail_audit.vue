ec83b918186897e951d6b98fa5aa2be582506d31
mangco <7912832+mangco@user.noreply.gitee.com>
2026-09-08 18:20:03 +0800

    文档预览地址调整

--- ruoyi-ui/src/views/lims/contract/detail_audit.vue
+++ ruoyi-ui/src/views/lims/contract/detail_audit.vue
@@ -64,7 +64,7 @@ export default {
   },
   methods: {
     handlePreview(f) {
-      var fileUrl = btoa('http://127.0.0.1:8080' + f.ossUrl);
+      var fileUrl = btoa('http://127.0.0.1:8082' + f.ossUrl);
       window.open('http://127.0.0.1:8012/onlinePreview?url=' + fileUrl, '_blank');
     },
    handleDownload(f) {
